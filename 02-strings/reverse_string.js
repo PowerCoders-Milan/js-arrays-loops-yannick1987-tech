b453839fa080ed3b78271e72e666243ea2e617c2
yannick1987-tech <ngassamnanayannick8@gmail.com>
2021-06-18 14:53:17 +0200

    exercise 2 done

--- 02-strings/reverse_string.js
+++ 02-strings/reverse_string.js
@@ -1,1 +1,8 @@
-// read carefully the instructions
+
+let myString = 'the quick brown fox jumps over the lazy dog';
+let stringArray = myString.split(' ');
+console.log(stringArray[3]);
+const reversed = stringArray.reverse();
+console.log('reversed:',reversed);
+stringArray.join(''),
+console.log(stringArray.join(''));
