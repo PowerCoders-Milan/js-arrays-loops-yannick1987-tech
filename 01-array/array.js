8f0feeefd64bfd974707160d929b100d11f37900
yannick1987-tech <ngassamnanayannick8@gmail.com>
2021-06-18 14:49:56 +0200

    exercise 1 done

--- 01-array/array.js
+++ 01-array/array.js
@@ -1,4 +1,4 @@
 // define an array with 5 elements
 const array = ['cameroon','gabon','nigeria','niger','congo','togo'];
 // console.log() the 3rd element
-console.log(array[0]);
+console.log(array[2]);
