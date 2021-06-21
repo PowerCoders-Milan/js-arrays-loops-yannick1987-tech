6c11580e75bd2cc5ebb2da3bc424f610cdd5a558
yannick1987-tech <ngassamnanayannick8@gmail.com>
2021-06-21 09:23:05 +0200

    exercise 3 done

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,11 @@
 // define your function here
+const multiplesOf = (number, limit) => {
+  for ( let i=1 ; i<15; i++){
+      if (i%2 === 0){
+        console.log(i)
+    
+      }
+  }  
+}
+
+multiplesOf(1,15);
